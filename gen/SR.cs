0cce69bd0ad85ad72bb918f9179e2c985dd3eb1f
Robert Pickering <robert.pickering@datadoghq.com>
2022-07-21 14:31:52 +0200

    first commit, nothing working

---
+++ gen/SR.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Text.RegularExpressions.Generator
+{
+    internal partial class SR
+    {
+        public static string Format(string x, object y)
+        {
+            return string.Format(x, y);
+        }
+
+
+        public static string Format(string x, object y, object z)
+        {
+            return string.Format(x, y, z);
+        }
+
+        public static string Format(string x, object y, object z, object a)
+        {
+            return string.Format(x, y, z, a);
+        }
+    }
+}
