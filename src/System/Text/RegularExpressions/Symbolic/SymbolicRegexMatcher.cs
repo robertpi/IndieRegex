9ed282a8645e5baeab5227ad942848d6a94b60c0
Robert Pickering <robert.pickering@datadoghq.com>
2022-07-26 06:34:17 +0200

    yet more cleaning

--- src/System/Text/RegularExpressions/Symbolic/SymbolicRegexMatcher.cs
+++ src/System/Text/RegularExpressions/Symbolic/SymbolicRegexMatcher.cs
@@ -165,42 +165,6 @@ namespace IndieSystem.Text.RegularExpressions.Symbolic
             return new SymbolicRegexMatcher<TSet>(builder, rootNode, captureCount, findOptimizations, matchTimeout);
         }
 
-#if NETFRAMEWORK || NETSTANDARD
-        private static ReadOnlySpan<byte> Log2DeBruijn => new byte[32]
-        {
-                    00, 09, 01, 10, 13, 21, 02, 29,
-                    11, 14, 16, 18, 22, 25, 03, 30,
-                    08, 12, 20, 28, 15, 17, 24, 07,
-                    19, 27, 23, 06, 26, 05, 04, 31
-        };
-
-        /// <summary>
-        /// Returns the integer (floor) log of the specified value, base 2.
-        /// Note that by convention, input value 0 returns 0 since Log(0) is undefined.
-        /// Does not directly use any hardware intrinsics, nor does it incur branching.
-        /// </summary>
-        /// <param name="value">The value.</param>
-        private static int Log2SoftwareFallback(uint value)
-        {
-            // No AggressiveInlining due to large method size
-            // Has conventional contract 0->0 (Log(0) is undefined)
-
-            // Fill trailing zeros with ones, eg 00010010 becomes 00011111
-            value |= value >> 01;
-            value |= value >> 02;
-            value |= value >> 04;
-            value |= value >> 08;
-            value |= value >> 16;
-
-            // uint.MaxValue >> 27 is always in range [0 - 31] so we use Unsafe.AddByteOffset to avoid bounds check
-            return Unsafe.AddByteOffset(
-                // Using deBruijn sequence, k=2, n=5 (2^5=32) : 0b_0000_0111_1100_0100_1010_1100_1101_1101u
-                ref MemoryMarshal.GetReference(Log2DeBruijn),
-                // uint|long -> IntPtr cast on 32-bit platforms does expensive overflow checks not needed here
-                (IntPtr)(int)((value * 0x07C4ACDDu) >> 27));
-        }
-#endif
-
 
         /// <summary>Constructs matcher for given symbolic regex.</summary>
         private SymbolicRegexMatcher(SymbolicRegexBuilder<TSet> builder, SymbolicRegexNode<TSet> rootNode, int captureCount, RegexFindOptimizations findOptimizations, TimeSpan matchTimeout)
@@ -218,7 +182,7 @@ namespace IndieSystem.Text.RegularExpressions.Symbolic
             // minterms or adds an extra bit with power-of-two minterms. The extra slot at index _minterms.Length is used to
             // represent an \n occurring at the very end of input, for supporting the \Z anchor.
 #if NETFRAMEWORK || NETSTANDARD
-            _mintermsLog = Log2SoftwareFallback((uint)_minterms.Length) + 1;
+            _mintermsLog = BitOperations.Log2SoftwareFallback((uint)_minterms.Length) + 1;
 #else
             _mintermsLog = BitOperations.Log2((uint)_minterms.Length) + 1;
 #endif
---
+++ src/additional/BitOperations.cs
@@ -0,0 +1,50 @@
+﻿#if NETFRAMEWORK || NETSTANDARD
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndieSystem.Text.RegularExpressions
+{
+    internal class BitOperations
+    {
+        private static ReadOnlySpan<byte> Log2DeBruijn => new byte[32]
+        {
+                    00, 09, 01, 10, 13, 21, 02, 29,
+                    11, 14, 16, 18, 22, 25, 03, 30,
+                    08, 12, 20, 28, 15, 17, 24, 07,
+                    19, 27, 23, 06, 26, 05, 04, 31
+        };
+
+        /// <summary>
+        /// Returns the integer (floor) log of the specified value, base 2.
+        /// Note that by convention, input value 0 returns 0 since Log(0) is undefined.
+        /// Does not directly use any hardware intrinsics, nor does it incur branching.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        internal static int Log2SoftwareFallback(uint value)
+        {
+            // No AggressiveInlining due to large method size
+            // Has conventional contract 0->0 (Log(0) is undefined)
+
+            // Fill trailing zeros with ones, eg 00010010 becomes 00011111
+            value |= value >> 01;
+            value |= value >> 02;
+            value |= value >> 04;
+            value |= value >> 08;
+            value |= value >> 16;
+
+            // uint.MaxValue >> 27 is always in range [0 - 31] so we use Unsafe.AddByteOffset to avoid bounds check
+            return Unsafe.AddByteOffset(
+                // Using deBruijn sequence, k=2, n=5 (2^5=32) : 0b_0000_0111_1100_0100_1010_1100_1101_1101u
+                ref MemoryMarshal.GetReference(Log2DeBruijn),
+                // uint|long -> IntPtr cast on 32-bit platforms does expensive overflow checks not needed here
+                (IntPtr)(int)((value * 0x07C4ACDDu) >> 27));
+        }
+    }
+}
+#endif
